0065fd9ae27520642a5aedd4e49cde9f451ffdb8
chicichich <pch6789@naver.com>
2021-06-26 01:51:03 +0900

    build: init

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: 0,
+    'import/no-unresolved': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+    'no-new': 0,
+    indent: ['error', 2],
+    'import/extensions': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        endOfLine: 'lf',
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        printWidth: 120,
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+        quoteProps: 'as-needed',
+        arrowParens: 'always',
+        vueIndentScriptAndStyle: false,
+      },
+    ],
+  },
+};

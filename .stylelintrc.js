461f2cc05fc13aefceba41d6df13f90de7630cf8
chicichich <pch6789@naver.com>
2021-07-01 22:37:52 +0900

    style: stylelint 수정

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: ['stylelint-config-standard'],
   plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['mixin', 'include'],
+      },
+    ],
+  },
 };

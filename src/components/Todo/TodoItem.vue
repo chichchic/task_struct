511af86cf64e27887735f0b6b0e83eec1cd69254
chicichich <pch6789@naver.com>
2021-07-01 18:16:13 +0900

    feat: todo-item component

---
+++ src/components/Todo/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="todo-item">
+    <el-checkbox class="checkbox" :modelValue="check" @change="updateCheck" size="medium" />
+    <el-input
+      placeholder="할 일을 입력해보세요"
+      :modelValue="input"
+      @input="updateInput"
+      clearable
+      @clear="$emit('updateInput', '')"
+    />
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    check: Boolean,
+    input: String,
+  },
+  methods: {
+    updateCheck(value) {
+      this.$emit('updateCheck', value);
+    },
+    updateInput(value) {
+      this.$emit('updateInput', value);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.todo-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: #ebebeb;
+  }
+
+  & .el-input__inner {
+    background-color: transparent;
+    border: none;
+  }
+
+  & .checkbox {
+    margin: 0 1rem 0 1rem;
+  }
+}
+</style>

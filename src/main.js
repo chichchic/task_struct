cddea760cb640d0ce3c9ee3605ad57e14828ccad
unknown <leeminsik999@gmail.com>
2021-07-01 21:50:25 +0900

    feat: 구글 auth 추가

--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase';
+
 createApp(App).use(store).use(router).mount('#app');
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.auth().onAuthStateChanged((fu) => store.commit('setFireUser', fu));

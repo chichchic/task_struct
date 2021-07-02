b89f0100b2f8085300be94fed71958a320cb682a
chicichich <pch6789@naver.com>
2021-07-02 12:49:37 +0900

    feat: i18n 적용

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from './router';
 import store from './store';
 import installElementPlus from './plugins/element';
 import '@/assets/style/main.scss';
+import i18n from './i18n';
 
 //primeVue
 const app = createApp(App);
-app.use(store).use(router);
+app.use(store).use(router).use(i18n);
 installElementPlus(app);
 app.mount('#app');

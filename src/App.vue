d5d30d00fb6a525f62a2b35fedf8c420b290f88d
chicichich <pch6789@naver.com>
2021-07-06 20:28:40 +0900

    feat: side menu 숨기기
    기획 수정 예정

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <el-button type="danger" icon="el-icon-s-unfold" circle @click="drawer = true"></el-button>
+  <!-- <el-button type="danger" icon="el-icon-s-unfold" circle @click="drawer = true"></el-button> -->
   <router-view />
-  <Menu :drawer="drawer" @changeDrawer="changeDrawer" />
+  <!-- <Menu :drawer="drawer" @changeDrawer="changeDrawer" /> -->
 </template>
 
 <script>
-import Menu from '@/components/Menu.vue';
+// NOTE: side-menu 기획 수정
+// import Menu from '@/components/Menu.vue';
 export default {
   name: 'App',
   components: {
-    Menu,
+    // Menu,
   },
   data: () => ({
     drawer: false,

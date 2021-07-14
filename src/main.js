3353a07336af2d1dcb1ec616ef38776109b81e6f
chicichich <pch6789@naver.com>
2021-07-14 20:04:05 +0900

    feat: calender view 달력 컴포넌트 ui 설정

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from '@/App.vue';
 import router from './router';
 import store from './store';
 import installElementPlus from './plugins/element';
+import { DatePicker } from 'v-calendar';
 import '@/assets/style/main.scss';
 import i18n from './i18n';
 import firebase from 'firebase';
@@ -23,5 +24,6 @@ firebase.auth().onAuthStateChanged((fu) => store.commit('setFireUser', fu));
 
 const app = createApp(App);
 app.use(store).use(router).use(i18n);
+app.component('DatePicker', DatePicker);
 installElementPlus(app);
 app.mount('#app');
---
+++ src/views/Calendar.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="calendar">
+    <DatePicker :select-attribute="selectAttribute" :attributes="attributes" @dayclick="dayclick" />
+  </section>
+</template>
+<script>
+export default {
+  data: () => ({
+    attributes: [
+      {
+        highlight: {
+          contentStyle: { border: '1px solid #F6797C' },
+        },
+        order: 1,
+        dates: new Date(),
+      },
+    ],
+    tab: 'done',
+  }),
+  computed: {
+    selectAttribute() {
+      const backgroundColor = this.tab === 'todo' ? '#FC9A9D' : '#7389FF';
+      return {
+        highlight: {
+          style: { backgroundColor },
+          contentStyle: { color: 'white' },
+        },
+      };
+    },
+  },
+  methods: {
+    dayclick(day) {
+      console.log(day);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.calendar {
+  .vc-day-content:focus {
+    background-color: inherit !important;
+  }
+}
+</style>

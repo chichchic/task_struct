56e487b6f4cae894c643b65a280120062e1444e8
chicichich <pch6789@naver.com>
2021-07-17 03:13:50 +0900

    feat: vuex에 loading 변경 기능 추가

--- src/store/modules/base.js
+++ src/store/modules/base.js
@@ -1,6 +1,18 @@
+import { ElLoading } from 'element-plus';
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
+  state: {
+    $loading: null,
+  },
+  mutations: {
+    setLoading(state, payload) {
+      if (!payload && state.$loading !== null) {
+        state.$loading.close();
+        state.$loading = null;
+        return;
+      }
+      state.$loading = ElLoading.service({ fullScreen: true });
+    },
+  },
   actions: {},
 };

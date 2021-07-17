7612bc227645af0899dff224b8aedc7d5feb7c9d
chicichich <pch6789@naver.com>
2021-07-17 15:57:59 +0900

    feat: 로그인데이터 firestore와 연동

--- src/components/common/signInWithGoogle.js
+++ src/components/common/signInWithGoogle.js
@@ -2,12 +2,31 @@ import firebase from 'firebase';
 import { useStore } from 'vuex';
 export default function signInWithGoogle() {
   const store = useStore();
+  const $firestore = firebase.firestore();
   const signIn = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().languageCode = 'ko';
     store.commit('base/setLoading', true);
     try {
-      await firebase.auth().signInWithPopup(provider);
+      const signInResult = await firebase.auth().signInWithPopup(provider);
+      const providerData = signInResult.user.providerData[0];
+      const { uid, email, displayName, phoneNumber, providerId } = providerData;
+      const users = $firestore.collection('users');
+      const doc = await users.doc(uid).get();
+      if (!doc.exists) {
+        await users.doc(uid).set({
+          email,
+          displayName,
+          phoneNumber,
+          providerId,
+          createdAt: new Date(),
+          withdraw: false,
+          withdrawTime: null,
+        });
+      } else {
+        const userData = doc.data();
+        store.commit('user/setUserInfo', { ...userData, uid });
+      }
     } catch (error) {
       console.error(error);
     } finally {
@@ -16,6 +35,7 @@ export default function signInWithGoogle() {
   };
   const signOut = () => {
     firebase.auth().signOut();
+    store.commit('user/setUserInfo', null);
   };
   return {
     signIn,

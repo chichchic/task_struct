d11e843e185cab350ecb7a8ba6c7b6511273fc1c
unknown <leeminsik999@gmail.com>
2021-07-19 22:13:53 +0900

    feat: 404 error page routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Account from '../views/Account.vue';
 import Error404 from '../views/404.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/account',
-    name: 'Account',
-    component: Account,
-  },
   {
     path: '/:catchAll(.*)*',
     component: Error404,

524fcb4a5603296cc0cc059f668a633c309cc3a8
chicichich <pch6789@naver.com>
2021-07-21 13:03:33 +0900

    feat: navigation 아이콘 표기 조건 수정

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,12 @@
   <section class="navigation">
     <div class="logo">LOGO</div>
     <i icon class="el-icon-date" v-if="currentRouteName === 'Todo'" @click.prevent="$router.push('Calendar')"></i>
-    <i icon class="el-icon-tickets" v-else @click.prevent="$router.push('Todo')"></i>
+    <i
+      icon
+      class="el-icon-tickets"
+      v-else-if="currentRouteName === 'Calendar'"
+      @click.prevent="$router.push('Todo')"
+    ></i>
     <i icon class="el-icon-user-solid" :class="{ disable: $store.state.user.uid === null }"></i>
   </section>
 </template>

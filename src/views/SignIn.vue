b0e1aa566cb537ee575498b816d676e2e802f489
chicichich <pch6789@naver.com>
2021-07-29 14:31:54 +0900

    feat: signin 페이지 내용 수정

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,7 +1,20 @@
 <template>
   <section class="sign-in">
-    <el-button @click="redirectSignIn">로그인</el-button>
-    <el-button @click="signOut">로그아웃</el-button>
+    <h1 class="mb-9">logo</h1>
+    <p class="mb-9">
+      {{ $t('default.login_wecolme_text_top') }}
+      <br />
+      <br />
+      <b>{{ $t('default.login_wecolme_text_impress') }}</b
+      >{{ $t('default.login_wecolme_text_impress_postfix') }}
+      <br />
+      {{ $t('default.login_wecolme_text_bottom') }}
+    </p>
+
+    <button class="sign-in-button" @click="redirectSignIn">
+      <img src="@/assets/images/googleSigninButton.png" alt="Google sign in" />
+      <div class="img-overlay"></div>
+    </button>
   </section>
 </template>
 <script>
@@ -28,3 +41,46 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.sign-in {
+  height: calc(100vh - 50px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-size: 20px;
+    text-align: center;
+  }
+
+  .mb-9 {
+    margin-bottom: 90px;
+  }
+
+  .sign-in-button {
+    border: none;
+    cursor: pointer;
+    position: relative;
+    display: block;
+    width: 191px;
+    height: 46px;
+    background-color: transparent;
+
+    img {
+      width: 100%;
+    }
+
+    &:hover .img-overlay {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 3px;
+      background-color: rgba(128, 128, 128, 0.3);
+    }
+  }
+}
+</style>
